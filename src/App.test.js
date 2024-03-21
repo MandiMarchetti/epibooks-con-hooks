036280c53a6d1b0f5c7990e05f0250160591d883
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-03-21 15:57:57 +0100

    update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+import Welcome from "../src/components/Welcome";
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// 1.Verifica che il componente Welcome venga montato correttamente
+test("renders welcome message", () => {
+  const { getByText } = render(<Welcome />);
+  const welcomeMessage = getByText(/Benvenuti in EpiBooks/i);
+  expect(welcomeMessage).toBeInTheDocument();
+});
+
+//2.Verifica che vengano effettivamente renderizzate tante bootstrap cards quanti sono i libri nel file json utilizzato.
+//3.Verifica che il componente CommentArea venga renderizzato correttamente.
+//4.Verifica, magari con più tests, che il filtraggio dei libri tramite navbar si comporti come previsto.
